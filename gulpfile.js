cdf21c0e04f29208e0285a563760ebfab6ecc1a6
Cerdeira <miguel.cerdeira@yrbrands.com>
2018-08-02 11:10:26 +0100

    Concatenated javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp'),
     log = require('fancy-log'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
-var coffeeSrcs = 'components/coffee/tagline.coffee';
+var coffeeSrcs = ['components/coffee/tagline.coffee'];
+var jsSrcs = [
+    'components/scripts/pixgrid.js',
+    'components/scripts/rclick.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+];
 
 gulp.task('coffee', function(){
     gulp.src(coffeeSrcs)
         .pipe(coffee({ bare: true })
             .on('error', log))
         .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function(){
+    gulp.src(jsSrcs)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
 });
