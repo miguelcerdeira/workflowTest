b9f3db17d8566413f19db26b14b00e73757a2bc2
Cerdeira <miguel.cerdeira@yrbrands.com>
2018-08-02 11:52:33 +0100

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     log = require('fancy-log'),
     coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
     concat = require('gulp-concat');
 
 var coffeeSrcs = ['components/coffee/tagline.coffee'];
@@ -21,5 +22,6 @@ gulp.task('coffee', function(){
 gulp.task('js', function(){
     gulp.src(jsSrcs)
         .pipe(concat('script.js'))
+        .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
 });
